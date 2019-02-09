8890514d00eda87723fe82862bdd65386b8a5859
jennyafish <jafish@andrew.cmu.edu>
2019-02-08 23:29:03 -0500

    began working on processing audio

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, jsonify
+import requests
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-        return render_template('./public/index.html')
+    return render_template('./public/index.html')
+    
+@app.route("/processaudio", methods=['PUT'])
+def process():
+	data = request.form
+	requests.put("https://speech.googleapis.com/v1/speech:longrunningrecognize", data)
+	
+	
+	
