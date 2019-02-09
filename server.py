632ad3bf05e1d55d056042b6c5607cd043e07a1d
Chittesh Ruthesh <Sumathi@Chitteshs-MBP.wv.cc.cmu.edu>
2019-02-08 22:48:43 -0500

    Front end things

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
-
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-        return "Hello World!"
+        return render_template('./public/index.html')
