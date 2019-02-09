2d2deeee024e0cdc85f478f9399005b14e9b541d
Hannah He <hannahhe18@gmail.com>
2019-02-09 04:01:34 -0500

    PUT REQUESTS WORK

--- server.py
+++ server.py
@@ -1,37 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 from flask import send_from_directory
+from CategoryClassification import isOffTopic
 import os
+import io
 
 app = Flask(__name__)
-
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 @app.route("/")
 def hello():
         return render_template('./public/index.html')
-def text():
+@app.route('/cat', methods=['POST'])
+def cat():
     print(request.form) #request.form, request.files
-    
-    if request.method == 'PUT' and 'GraphImage' in request.files:
-        photo = request.files['GraphImage']
-        in_memory_file = io.BytesIO()
-        photo.save(in_memory_file)
-        data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
-        color_image_flag = 1
-        img = cv2.imdecode(data, color_image_flag)
-    if(isinstance(graph1, GraphTheory)):
-        laTexCode = graph1.detectPointsAndAxes(img, numEdges)
-    if(isinstance(graph1, ScatterPlot)):
-        laTexCode = graph1.detectPointsAndAxes(img)
-    if(isinstance(graph1, Function)):
-        D1 =  request.form["D1"]
-        D2 =  request.form["D2"]
-        X1 =  request.form["X1"]
-        X2 =  request.form["X2"]
-        Y1 =  request.form["Y1"]
-        Y2 =  request.form["Y2"]
-        laTexCode = graph1.plotFunction(func, D1, D2, X1, X2, Y1, Y2)
-
-
-    return jsonify({'latex': laTexCode}) #jsonify: turns python dictionaries into json
+    category = request.form["cat"]
+    offTopic = isOffTopic(category, "seaweed puffer fish sharks whales i like hobbies and leisure seahorses swimming through the water. submarines exploring plankton. Something fish swim bubbles i go to the beach ocean things she sells sea shells by the seashore spongebob patrick the star barnacle boy")
+    print("offTopic", offTopic)
+    return jsonify({'offT': offTopic})
 
 @app.route('/favicon.ico')
 def favicon():
--- templates/src/App.js
+++ templates/src/App.js
@@ -1,75 +1,52 @@
-import React, { Component } from 'react';
-import { ReactMic } from 'react-mic';
 import "babel-polyfill";
 import $ from "jquery";
 
+import React from "react";
 import ReactDOM from "react-dom";
 import Clipboard from "clipboard";
 
 import { Dropdown, Button, NavItem, Modal, Row, Col, Input } from "react-materialize";
 
-export class UserInput extends React.Component {
+class UserInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
     this.state.formData = new FormData();
    }
-   onChange = (e, val) => { //defines a function, onChange which takes an event and value and takes value and stores it in something
-     this.sate.formData.set('Text', 'Hello World')
-     this.forceUpdate();
-   }
-   onChangeField = field => {
-       let onChangeSpecific = (e) => {
-         let value = e.target.value;
-         this.state.formData.set(field, value);
-       }
-       return onChangeSpecific
-   }
-
-  getText = (e) => { //get the input text
-    let text = e.target.value;
-    this.state.text = text;
-    console.log('input text', text);
-    this.state.formData.set('Text', this.state.text);
-  }
-  sendTextInfo = () => {
-    fetch('/text', { //http put request
-      method: 'PUT',
-      body: this.state.formData
-    })
-    .then(response => response.json()) //get back a json
-    .catch(error => console.error(error)) //error handling
-    //.then(response => {
-      //this.state.laTeX = response.latex;
-      //console.log(this.state.laTeX);
-      //console.log(this.state);
-      this.forceUpdate();
-    //}); //if the json is valid
+  getCat = (e) => { //get the input text
+    let cat = e.target.value;
+    this.state.cat = cat;
+    console.log('category', cat);
+    this.state.formData.set('cat', this.state.cat);
+    console.log('formData', this.state.formData.get('cat'));
   }
 
-
+  sendCategoryInfo = () => {
+      fetch('/cat', { //http put request
+        method: 'POST',
+        body: this.state.formData
+      })
+    //  .then(response => response.json()) //get back a json
+      //.catch(error => console.error(error)) //error handling
+      .then(response => {
+        //this.state.cat = response.offT;
+        //console.log(this.state.cat);
+        //console.log(this.state);
+        this.forceUpdate();
+      }); //if the json is valid
+    }
   render() {
     return (
       <div>
+        <h1 className='title'>focus</h1>
         <Row>
-        <Col s={1}>Please input your Text:</Col>
-        <Col s={5}><Input placeholder="Text" defaultValue='' s={6} onChange ={this.onChangeField('Text')}/></Col>
+        <Input placeholder="Text Here" defaultValue="" s={12} onChange = {this.getCat} />
+         <Button waves='light' onClick = {this.sendCategoryInfo}>Submit</Button>
         </Row>
+        <footer className = 'footer'> By Hannah He </footer>
       </div>
     );
   }
 }
 
-$(document).ready(() => {
-  ReactDOM.render(
-    <div>
-      <h1 className='title'>focus</h1>
-      <UserInput />
-      <footer className = 'footer'> By Hannah He </footer>
-    </div>,
-      //<Hannah number={1} />
-      //<Hannah number={2} />
-    //</div>,
-    document.getElementById("root")
-  );
-});
+export default UserInput;
