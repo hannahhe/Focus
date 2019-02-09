b6e223887ecc3f5433da2a464535dd05224e3303
Hannah He <hannahhe18@gmail.com>
2019-02-08 23:07:22 -0500

    added favicon in tab

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request, jsonify
+from flask import send_from_directory
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
         return render_template('./public/index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
