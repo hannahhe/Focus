a504955741d142ca1dae0405b1d623a05313df91
Clarissa Xu <clarissaxu@Clarissas-MBP.wv.cc.cmu.edu>
2019-02-08 23:18:58 -0500

    FIXED MY FUNCTIONS WOOHOO

--- GoogleCloud/CategoryClassification.py
+++ GoogleCloud/CategoryClassification.py
@@ -2,22 +2,38 @@ import six
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
-text = "seaweed puffer fish sharks whales seahorses swimming through the water. submarines exploring plankton. Something fish swim bubbles i go to the beach ocean things she sells sea shells by the seashore spongebob patrick the star barnacle boy"
+text1 = "seaweed puffer fish sharks whales seahorses swimming through the water. submarines exploring plankton. Something fish swim bubbles i go to the beach ocean things she sells sea shells by the seashore spongebob patrick the star barnacle boy"
 
-client = language.LanguageServiceClient()
+def analyze(text):
+    client = language.LanguageServiceClient()
 
-if isinstance(text, six.binary_type):
-	text = text.decode('utf-8')
+    if isinstance(text, six.binary_type):
+            text = text.decode('utf-8')
 
-document = types.Document(
-	content=text.encode('utf-8'),
-	type=enums.Document.Type.PLAIN_TEXT)
+    document = types.Document(
+            content=text.encode('utf-8'),
+            type=enums.Document.Type.PLAIN_TEXT)
 
-categories = client.classify_text(document).categories
+    categories = client.classify_text(document).categories
 
-for category in categories:
-	print(u'=' * 20)
-	print(u'{:<16}: {}'.format('name', category.name))
-	print(u'{:<16}: {}'.format('confidence', category.confidence))
+    for category in categories:
+            print(u'=' * 20)
+            print(u'{:<16}: {}'.format('name', category.name))
+            print(u'{:<16}: {}'.format('confidence', category.confidence))
 
-print("End")
+def classify_file(gcs_uri):
+    """Classifies content categories of the text in a Google Cloud Storage
+    file.
+    """
+    client = language.LanguageServiceClient()
+
+    document = types.Document(
+        gcs_content_uri=gcs_uri,
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    categories = client.classify_text(document).categories
+
+    for category in categories:
+        print(u'=' * 20)
+        print(u'{:<16}: {}'.format('name', category.name))
+        print(u'{:<16}: {}'.format('confidence', category.confidence))
--- GoogleCloud/SentimentAnalysis.py
+++ GoogleCloud/SentimentAnalysis.py
@@ -2,18 +2,19 @@
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
-print("hello")
-# Instantiates a client
-client = language.LanguageServiceClient()
 
-# The text to analyze
-text = u'Hello, world!'
-document = types.Document(
-    content=text,
-    type=enums.Document.Type.PLAIN_TEXT)
+def analyze(text):
+    # Instantiates a client
+    client = language.LanguageServiceClient()
 
-# Detects the sentiment of the text
-sentiment = client.analyze_sentiment(document=document).document_sentiment
+    # The text to analyze
+    text = u'Hello, world!'
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
 
-print('Text: {}'.format(text))
-print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+    print('Text: {}'.format(text))
+    print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
