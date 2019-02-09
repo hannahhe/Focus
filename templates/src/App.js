289a7e4e2c42a90f64d500a59705903a3a496092
Chittesh Ruthesh <Sumathi@Chitteshs-MBP.wv.cc.cmu.edu>
2019-02-09 13:13:44 -0500

    worked on dropdown integration

--- templates/src/App.js
+++ templates/src/App.js
@@ -6,6 +6,7 @@ import ReactDOM from "react-dom";
 //import Clipboard from "clipboard";
 import "./WebAudioRecorder.min.js";
 import { scaryAnimals } from "./categories";
+import Select from 'react-select';
 import { Dropdown, Button, NavItem, Modal, Row, Col, Input } from "react-materialize";
 
 class UserInput extends React.Component {
@@ -76,5 +77,63 @@ class UserInput extends React.Component {
     );
   }
 }
+
+/*class App extends React.Component {
+  state = {
+    selectedOption: null,
+  }
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption.label);
+
+  }
+  componentDidMount() {
+    navigator.mediaDevices.getUserMedia({ //get access to user's microphone/cam
+      'audio': true,
+      'video': false, //don't need cam
+    } ).then((stream) => {
+      const audioContext = new AudioContext(); //OBJECT
+      const input = audioContext.createMediaStreamSource(stream); //Get a stream source
+      const recorder = new WebAudioRecorder(input, { //from library
+        'workerDir': '/static/', //1. provides audio reorder and access
+        'numChannels': 1, //want monosound and not stero
+      }); //CHROME AND FIREFOX HAPPY
+      recorder.onComplete = (rec, blob) => {  //what to do when finished recording?
+        const url = '/speechdata';
+        fetch(url, {
+          method: 'POST', //SHOVE TO SERVER
+          body: blob,
+        });
+      };
+      recorder.startRecording();
+      setInterval(() => { recorder.finishRecording(); recorder.startRecording();}, 5000);
+      //setTimeout(() => { recorder.finishRecording(); }, 2000); //set how long it's recording
+    }).catch((err) => {
+      console.log("There was an error", err); //WHAT HAPPENS WHEN YOU'RE MICLESS?
+    });
+  }
+  render() {
+    const { selectedOption } = this.state;
+    return (
+      <div className="App">
+        <img src={'../../static/img/1.png'}/>
+        <Row>
+
+
+          <ul>
+          <li class = "topicText">What do you want to talk about?</li>
+          </ul>
+          <Select value={selectedOption}
+            onChange={this.handleChange}
+            options={scaryAnimals}/>
+        </Row>
+        <div>
+          {selectedOption != null ?
+            <RecordButton />:"" }
+        </div>
+      </div>
+    );
+  }
+}*/
 
 export default UserInput;
